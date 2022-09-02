09927ddbe36d6646a25aa3f076be4487b62abc54
SheerSt <8891626+SheerSt@users.noreply.github.com>
2022-09-02 16:38:47 -0500

    various

--- Player.cs
+++ Player.cs
@@ -7,9 +7,12 @@ using Godot;
 public class Player : KinematicBody2D
 {
 
+	String animationName;
+	String previousDirectionName = "down";
 	float speed;
 	Vector2 direction = new Vector2();
 	AudioStreamOGGVorbis bumpSound;
+	AudioStreamOGGVorbis walkSound;
 	Sprite sprite;
 	AnimationPlayer animationPlayer;
 	AudioStreamPlayer2D audioPlayer;
@@ -24,10 +27,13 @@ public class Player : KinematicBody2D
 
 		bumpSound = ResourceLoader.Load<AudioStreamOGGVorbis>("bump2.ogg");
 		bumpSound.Loop = false;
+		walkSound = ResourceLoader.Load<AudioStreamOGGVorbis>("walk1.ogg");
+		walkSound.Loop = false;
+
 		sprite = (Sprite)FindNode("Sprite");
 		animationPlayer = (AnimationPlayer)FindNode("AnimationPlayer");
 		audioPlayer = GetNode<AudioStreamPlayer2D>("/root/Game/AudioStreamPlayer2D");
-        collisionShape2D = (CollisionShape2D)FindNode("CollisionShape2D");
+        //collisionShape2D = (CollisionShape2D)FindNode("CollisionShape2D");
 
 		// TODO: move somewhere
 		background = GetNode<Sprite>("/root/Game/Background");
@@ -39,33 +45,55 @@ public class Player : KinematicBody2D
 		bumps = new int[width][];
 		// Get bump map offsets.
 		float sum = 0f;
-		for (int k = 0; k < width; ++k)
+        for (int k = 0; k < width; ++k)
         {
 
-			bumps[k] = new int[height];
-			for (int l = 0; l < height; ++l)
-			{
+            bumps[k] = new int[height];
+            for (int l = 0; l < height; ++l)
+            {
 
-				for (int i = -4; i < 8; ++i)
-				{
-					int xPos = k + i;
-					if (xPos < 0 || xPos >= width) continue;
+                for (int i = -4; i < 8; ++i)
+                {
+                    int xPos = k + i;
+                    if (xPos < 0 || xPos >= width) continue;
 
-					for (int j = -4; j < 8; ++j)
-					{
-						int yPos = l + j;
-						if (yPos < 0 || yPos >= height) continue;
+                    for (int j = -4; j < 8; ++j)
+                    {
+                        int yPos = l + j;
+                        if (yPos < 0 || yPos >= height) continue;
 
-						Color color = bumpMap.GetPixel(xPos, yPos);
-						sum += Math.Abs(color.r - color.g) + Math.Abs(color.g - color.b) + Math.Abs(color.b - color.r);
+                        Color color = bumpMap.GetPixel(xPos, yPos);
+                        sum += Math.Abs(color.r - color.g) + Math.Abs(color.g - color.b) + Math.Abs(color.b - color.r);
 
-					}
+                    }
 
-				}
-				sum = sum / 32f;
-				bumps[k][l] = (int)sum;
+                }
+                sum = sum / 32f;
+                bumps[k][l] = (int)sum;
 
-			}
+            }
+
+        }
+
+		// Offset sprite based on terrain.
+		OffsetSprite();
+
+	}
+
+    /**
+	 * Offset sprite based on terrain.
+	 */
+    public void OffsetSprite()
+    {
+
+		Vector2 newPos = new Vector2(Position.x, Position.y) - background.Position;
+		int xPos = (int)newPos.x;
+		int yPos = (int)newPos.y;
+		if (0 < xPos && xPos < bumps.Length && 0 < yPos && yPos < bumps[xPos].Length)
+		{
+
+			int offsetY = bumps[xPos][yPos];
+			this.sprite.Position = new Vector2(0, 4 - offsetY);
 
 		}
 
@@ -73,33 +101,42 @@ public class Player : KinematicBody2D
 
     public override void _Process(float delta)
     {
+		if (Input.IsKeyPressed((int)Godot.KeyList.X))
+		{
 
-		if (Input.IsKeyPressed((int)Godot.KeyList.X)) speed = 1.5f;
-		else speed = 1f;
+			animationName = "run-";
+			speed = 1.5f;
 
-		String animationName = "walk-";
+		}
+		else
+		{
+			animationName = "walk-";
+			speed = 1f;
+		}
+
         direction = new Vector2(0, 0);
 
+		String directionName = "";
 		if (Input.IsKeyPressed((int)Godot.KeyList.Up))
 		{
-			animationName += "up";
+			directionName += "up";
 			direction.y -= 1;
 		}
 		else if (Input.IsKeyPressed((int)Godot.KeyList.Down))
 		{
-			animationName += "down";
+			directionName += "down";
 			direction.y += 1;
 		}
 		if (Input.IsKeyPressed((int)Godot.KeyList.Right))
 		{
 			//sprite.FlipH = false;
-			animationName += "right";
+			directionName += "right";
 			direction.x += 1;
 		}
 		else if (Input.IsKeyPressed((int)Godot.KeyList.Left))
 		{
 			//sprite.FlipH = false;
-			animationName += "left";
+			directionName += "left";
 			direction.x -= 1;
 		}
 
@@ -108,6 +145,9 @@ public class Player : KinematicBody2D
 
 		if (direction != Vector2.Zero)
 		{
+
+			previousDirectionName = directionName;
+			animationName += previousDirectionName;
 
 			if (!animationPlayer.CurrentAnimation.Equals(animationName))
 			{
@@ -125,33 +165,53 @@ public class Player : KinematicBody2D
 
 			}
 
-			Vector2 moveAmount = MoveAndSlide(direction * speed * 80);
+			Vector2 moveAmount = MoveAndSlide(direction * speed * 65);
+
 			KinematicCollision2D collision = GetLastSlideCollision();
-			if (collision != null && !audioPlayer.Playing) {
-
-				speed = 1;
-				audioPlayer.Stream = bumpSound;
-				audioPlayer.Play();
-
-			}
-
-			Vector2 newPos = new Vector2(Position.x, Position.y) - background.Position;
-			int xPos = (int)newPos.x;
-			int yPos = (int)newPos.y;
-			if (0 < xPos && xPos < bumps.Length && 0 < yPos && yPos < bumps[xPos].Length)
+            if (collision != null && !audioPlayer.Playing)
             {
 
-				int offsetY = bumps[xPos][yPos];
-				this.sprite.Position = new Vector2(0, -offsetY);
+                speed = 1;
+                audioPlayer.Stream = bumpSound;
+                //audioPlayer.VolumeDb = 0.5f;
+                audioPlayer.Play();
+
+            }
+
+
+			// Walk sound
+			AudioStreamPlayer player = (AudioStreamPlayer)FindNode("Sounds");
+			if (!player.Playing)
+			{
+
+				//audioPlayer.Stream = walkSound;
+				//audioPlayer.Play();
+
+				AudioStreamOGGVorbis sound;
+				if (speed > 1) sound = ResourceLoader.Load<AudioStreamOGGVorbis>("run1.ogg");
+				else sound = ResourceLoader.Load<AudioStreamOGGVorbis>("walk2.ogg");
+				sound.Loop = false;
+				player.Stream = sound;
+				player.Play();
+
 
 			}
+
+			// Offset sprite based on terrain.
+			OffsetSprite();
 
 		}
 		else
         {
 
-			if (animationPlayer.IsPlaying()) animationPlayer.Seek(0, true);
-			animationPlayer.Stop(true);
+			// Test
+			animationName = "idle-";
+			animationName += previousDirectionName;
+			animationPlayer.Play(animationName);
+
+			//
+			//if (animationPlayer.IsPlaying()) animationPlayer.Seek(0, true);
+			//animationPlayer.Stop(true);
 
 		}
 
