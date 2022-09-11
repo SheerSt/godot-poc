a4b2c2a89226577cb47b183636405478e300375a
SheerSt <8891626+SheerSt@users.noreply.github.com>
2022-09-11 18:05:01 -0500

    before polygon2d

--- Trees.cs
+++ Trees.cs
@@ -10,6 +10,40 @@ public class Trees : TileMap
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+
+		TileMap shadowTileMap = GetNode<TileMap>("/root/Game/Shadows");
+
+		// For each tile
+		foreach (Vector2 position in GetUsedCells())
+		{
+
+			// Make a new Sprite that copies the tile texture
+			int index = GetCellv(position);
+			Texture texture = this.TileSet.TileGetTexture(index);
+			Sprite sprite = new Sprite();
+			sprite.Texture = texture;
+			sprite.Position = MapToWorld(position);
+			sprite.RegionEnabled = true;
+			sprite.RegionRect = this.TileSet.TileGetRegion(index);
+
+			//GD.Print(sprite.RegionRect);
+
+			// Color sprite to be black, with slight transparency.
+			//sprite.Modulate = new Color(0f, 0f, 0f, 1f);
+
+			// Rotate 125 degrees, and squash it.
+			//sprite.Rotation = Mathf.Deg2Rad(180);
+			sprite.Scale = new Vector2(1f, 1f);
+			sprite.FlipV = true;
+
+			// Add the sprite as a child to Shadows TileMap
+			shadowTileMap.AddChild(sprite);
+
+
+		}
+
+
+
 	}
 
 	public override void _Process(float delta)
