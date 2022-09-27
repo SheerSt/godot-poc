6fa62785731c368ab1db2b8fd7a2858157daa9db
SheerSt <8891626+SheerSt@users.noreply.github.com>
2022-09-27 15:51:53 -0500

    remove unused files; fix Tiles dir capitalization

--- tiles/tileset1.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="tileset1" tilewidth="16" tileheight="16" tilecount="12" columns="3">
- <image source="tileset1.png" width="63" height="64"/>
-</tileset>
