2a429e6a5bc01c023e0c0e7b8dac2a6f5eefcea4
SheerSt <8891626+SheerSt@users.noreply.github.com>
2022-10-02 17:08:04 -0500

    save wor

--- Scripts/Monster.cs
+++ Scripts/Monster.cs
@@ -7,6 +7,7 @@ public class Monster : KinematicBody2D
 
 	Direction direction = Direction.NONE;
 	Timer moveTimer;
+	Timer dirtTimer;
 	float speed = 1f;
 	float walkingTimer = 0f;
 	Sprite sprite;
@@ -17,6 +18,7 @@ public class Monster : KinematicBody2D
 	{
 
 		moveTimer = (Timer)FindNode("MoveTimer");
+		dirtTimer = (Timer)FindNode("DirtTimer");
 		sprite = (Sprite)FindNode("Sprite");
 		animationPlayer = (AnimationPlayer)FindNode("AnimationPlayer");
 
@@ -33,18 +35,55 @@ public class Monster : KinematicBody2D
 
 			// Snap the sprite's position to whole numbers, otherwise it looks weird.
 			// There's likely a better way to do this.
-			sprite.Position = Position.Round() - Position;
+			sprite.Position = Position.Floor() - Position;
+
 		}
 		else if (this.moveTimer.IsStopped())
 		{
 			this.moveTimer.Start();
 			animationPlayer.Stop();
+			this.dirtTimer.Paused = true;
 		}
+
+	}
+
+	private void _on_DirtTimer_timeout()
+	{
+
+		//
+		PackedScene footPrints = GD.Load<PackedScene>("res://Monsters/Dirt-Trail.tscn");
+		Node node = footPrints.Instance();
+		Sprite sprite = (Sprite)node.FindNode("Sprite");
+		sprite.Position = Position.Floor() + new Vector2(0, 2);  // y = 8 - yOffset
+		AnimationPlayer animationPlayer = (AnimationPlayer)node.FindNode("AnimationPlayer");
+		animationPlayer.CurrentAnimation = "footprints-fade-sand";
+		animationPlayer.Play();
+
+		//
+		Node2D map = (Node2D)Game.instance.FindNode("Map");
+		TileMap tileMap = (TileMap)map.FindNode("TileMap");
+		map.AddChildBelowNode(tileMap, node);
+
+
+		//
+		PackedScene dirt = GD.Load<PackedScene>("res://Monsters/debris1.tscn");
+		node = dirt.Instance();
+		//((Node2D)node).Position = Position.Floor() + new Vector2(0, 16);  // y = 8 - yOffset
+		((Node2D)node).Position = new Vector2(0, 4);
+		animationPlayer = (AnimationPlayer)node.FindNode("AnimationPlayer");
+		animationPlayer.CurrentAnimation = "Anim1";
+		animationPlayer.Play();
+		//tileMap = (TileMap)map.FindNode("Trees");
+		this.AddChild(node);
 
 	}
 
 	private void _on_MoveTimer_timeout()
 	{
+
+		//
+		dirtTimer.Paused = false;
+		dirtTimer.Start();
 
 		walkingTimer = 1f;
 
@@ -68,4 +107,3 @@ public class Monster : KinematicBody2D
 	}
 
 }
-
