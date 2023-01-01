c992b3859fd71165647c28d907358c6a6ca9f1a4
SheerSt <8891626+SheerSt@users.noreply.github.com>
2023-01-01 14:35:59 -0600

    tweak / fix monster animations

--- Monsters/Diglett/Diglett.cs
+++ Monsters/Diglett/Diglett.cs
@@ -4,71 +4,70 @@ using System;
 public partial class Diglett : Monster
 {
 
-    Timer dirtTimer;
-    AudioStreamPlayer2D dirtSound;
+	Timer dirtTimer;
+	AudioStreamPlayer2D dirtSound;
 
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
 
-        base._Ready();
-        dirtTimer = (Timer)FindChild("DirtTimer");
-        dirtSound = (AudioStreamPlayer2D)FindChild("DirtSound");
+		base._Ready();
+		dirtTimer = (Timer)FindChild("DirtTimer");
+		dirtSound = (AudioStreamPlayer2D)FindChild("DirtSound");
 
-    }
+	}
 
-    public override void _Process(double delta)
-    {
+	public override void _Process(double delta)
+	{
 
-        base._Process(delta);
+		base._Process(delta);
 
-        if (this.walkingTimer.IsStopped() && this.moveTimer.IsStopped())
-            this.dirtTimer.Paused = true;
+		if (this.walkingTimer.IsStopped() && this.moveTimer.IsStopped())
+			this.dirtTimer.Paused = true;
 
-    }
-    private void _on_DirtTimer_timeout()
-    {
+	}
+	private void _on_DirtTimer_timeout()
+	{
 
-        //
-        PackedScene footPrints = GD.Load<PackedScene>("res://Monsters/Dirt-Trail.tscn");
-        Node node = footPrints.Instantiate();
-        //Sprite2D sprite = (Sprite2D)node.FindNode("Sprite2D");
-        //sprite.Position = Position.Floor() + new Vector2(0, 2);  // y = 8 - yOffset
-        ((Node2D)node).Position = Position.Floor() + new Vector2(0, -5);
-        AnimationPlayer animationPlayer = (AnimationPlayer)node.FindChild("AnimationPlayer");
-        animationPlayer.CurrentAnimation = "footprints-fade-sand";
-        animationPlayer.Play();
+		//
+		PackedScene footPrints = GD.Load<PackedScene>("res://Monsters/Dirt-Trail.tscn");
+		Node node = footPrints.Instantiate();
+		//Sprite2D sprite = (Sprite2D)node.FindNode("Sprite2D");
+		//sprite.Position = Position.Floor() + new Vector2(0, 2);  // y = 8 - yOffset
+		((Node2D)node).Position = Position.Floor() + new Vector2(0, -5);
+		AnimationPlayer animationPlayer = (AnimationPlayer)node.FindChild("AnimationPlayer");
+		animationPlayer.CurrentAnimation = "footprints-fade-sand";
+		animationPlayer.Play();
 
-        //
-        Node2D map = (Node2D)Game.instance.FindChild("Map");
-        TileMap tileMap = (TileMap)map.FindChild("Trees");
-        tileMap.AddChild(node);  // BelowNode(tileMap, 
+		//
+		Node2D map = (Node2D)Game.instance.FindChild("Map");
+		TileMap tileMap = (TileMap)map.FindChild("Trees");
+		tileMap.AddChild(node);  // BelowNode(tileMap, 
 
+		//
+		PackedScene dirt = GD.Load<PackedScene>("res://Monsters/debris1.tscn");
+		node = dirt.Instantiate();
+		//((Node2D)node).Position = Position.Floor() + new Vector2(0, 16);  // y = 8 - yOffset
+		((Node2D)node).Position = new Vector2(0, 4);
+		animationPlayer = (AnimationPlayer)node.FindChild("AnimationPlayer");
+		animationPlayer.CurrentAnimation = "Anim1";
+		animationPlayer.Play();
+		//tileMap = (TileMap)map.FindNode("Trees");
+		this.AddChild(node);
 
-        //
-        PackedScene dirt = GD.Load<PackedScene>("res://Monsters/debris1.tscn");
-        node = dirt.Instantiate();
-        //((Node2D)node).Position = Position.Floor() + new Vector2(0, 16);  // y = 8 - yOffset
-        ((Node2D)node).Position = new Vector2(0, 4);
-        animationPlayer = (AnimationPlayer)node.FindChild("AnimationPlayer");
-        animationPlayer.CurrentAnimation = "Anim1";
-        animationPlayer.Play();
-        //tileMap = (TileMap)map.FindNode("Trees");
-        this.AddChild(node);
+	}
 
-    }
+	private void _on_MoveTimer_timeout()
+	{
 
-    private void _on_MoveTimer_timeout()
-    {
+		base._on_MoveTimer_timeout();
 
-        base._on_MoveTimer_timeout();
+		//
+		dirtTimer.Paused = false;
+		dirtTimer.Start();
+		dirtSound.Play();
 
-        //
-        dirtTimer.Paused = false;
-        dirtTimer.Start();
-        dirtSound.Play();
-
-    }
+	}
 
 
 }
--- Scripts/Monster.cs
+++ Scripts/Monster.cs
@@ -5,66 +5,66 @@ using System;
 public partial class Monster : CharacterBody2D
 {
 
-    public Direction direction = Direction.NONE;
-    public float speed = 1f;
-    public Sprite2D sprite;
-    public AnimationPlayer animationPlayer;
-    public Timer moveTimer;
-    public Timer walkingTimer;
+	public Direction direction = Direction.NONE;
+	public float speed = 1f;
+	public Sprite2D sprite;
+	public AnimationPlayer animationPlayer;
+	public Timer moveTimer;
+	public Timer walkingTimer;
 
-    public override void _Ready()
-    {
-        
-        moveTimer = (Timer)FindChild("MoveTimer");
-        walkingTimer = (Timer)FindChild("WalkingTimer");
-        sprite = (Sprite2D)FindChild("Sprite2D");
-        animationPlayer = (AnimationPlayer)FindChild("AnimationPlayer");
+	public override void _Ready()
+	{
+		
+		moveTimer = (Timer)FindChild("MoveTimer");
+		walkingTimer = (Timer)FindChild("WalkingTimer");
+		sprite = (Sprite2D)FindChild("Sprite2D");
+		animationPlayer = (AnimationPlayer)FindChild("AnimationPlayer");
 
-    }
-    
-    public override void _Process(double delta)
-    {
+	}
+	
+	public override void _Process(double delta)
+	{
 
-        if (!this.walkingTimer.IsStopped())
-        {
+		if (!this.walkingTimer.IsStopped())
+		{
 
-            Velocity = this.direction.getVector().Normalized() * this.speed * 36;
-            MoveAndSlide();
+			Velocity = this.direction.getVector().Normalized() * this.speed * 36;
+			MoveAndSlide();
 
-            // Snap the sprite's position to whole numbers, otherwise it looks weird.
-            // There's likely a better way to do this.
-            sprite.Position = Position.Floor() - Position;
+			// Snap the sprite's position to whole numbers, otherwise it looks weird.
+			// There's likely a better way to do this.
+			sprite.Position = Position.Floor() - Position;
 
-        }
-        else if (this.moveTimer.IsStopped())
-        {
-            this.moveTimer.Start();
-            animationPlayer.Stop();
-        }
+		}
+		else if (this.moveTimer.IsStopped())
+		{
+			this.moveTimer.Start();
+			animationPlayer.Stop();
+		}
 
-    }
+	}
 
-    public void _on_MoveTimer_timeout()
-    {
-        walkingTimer.Start();
+	public void _on_MoveTimer_timeout()
+	{
+		walkingTimer.Start();
 
-        // Get a random direction;
-        this.direction = DirectionExtensions.all[Game.random.Next(0, DirectionExtensions.all.Length)];
-        animationPlayer.CurrentAnimation = "Move";
-        animationPlayer.Play();
+		// Get a random direction;
+		this.direction = DirectionExtensions.all[Game.random.Next(0, DirectionExtensions.all.Length)];
+		animationPlayer.CurrentAnimation = "Move";
+		animationPlayer.Play();
 
-        // Set sprite direction based this.direction
-        this.sprite.FlipH = false;
-        Vector2 directionVector = this.direction.getVector();
-        int offsetY = 0;
-        if (directionVector.x > 0)
-            this.sprite.FlipH = true;
-        if (directionVector.y < 0)
-            offsetY = 1;
-        else if (directionVector.y > 0)
-            offsetY = 2;
-        this.sprite.FrameCoords = new Vector2i(0, offsetY);
+		// Set sprite direction based this.direction
+		this.sprite.FlipH = false;
+		Vector2 directionVector = this.direction.getVector();
+		int offsetY = 0;
+		if (directionVector.x > 0)
+			this.sprite.FlipH = true;
+		if (directionVector.y < 0)
+			offsetY = 1;
+		else if (directionVector.y > 0)
+			offsetY = 2;
+		this.sprite.FrameCoords = new Vector2i(0, offsetY);
 
-    }
+	}
 
 }
